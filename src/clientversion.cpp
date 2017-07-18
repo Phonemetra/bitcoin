d509cefde54f66754f6a8763841e8b8a0ee9fd85
roberto73c <roberto.cesina@phonemetra.com>
2017-07-18 02:13:16 +0200

    0.0.1
    0.0.1

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -42,10 +42,11 @@ const std::string CLIENT_NAME("Satoshi");
 #include "build.h"
 #endif
 
-//! git will put "#define GIT_ARCHIVE 1" on the next line inside archives. $Format:%n#define GIT_ARCHIVE 1$
+//! git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
+#define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#define GIT_COMMIT_ID "$Format:%h$"
-#define GIT_COMMIT_DATE "$Format:%cD$"
+#define GIT_COMMIT_ID "75b5643c47c"
+#define GIT_COMMIT_DATE "Mon, 17 Jul 2017 16:54:20 -0700"
 #endif
 
 #define BUILD_DESC_WITH_SUFFIX(maj, min, rev, build, suffix) \
